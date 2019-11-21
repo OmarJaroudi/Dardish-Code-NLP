c8f0935f164d1f43197b0f800607c8a5d9a37b11
Mohamad Abou Harb <20209926+TwoShock@users.noreply.github.com>
2019-11-21 23:12:41 +0200

    addedd new pickle files and my main file
    note pickle file contains the  raw comments and code blocks as well as tokenized comments with function signature list
    function signature list is a two tuple containing function name which is a string as the first element and a list of parameters as second element in tuple. Use whatever you see fit

---
+++ main.py
@@ -0,0 +1,76 @@
+"""
+Created on Thu Nov 21 18:17:07 2019
+
+@author: Mohamad Abou Harb
+"""
+import re 
+import pickle
+import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from utils import FunctionSignatureExtractor
+
+def loadPickleFile(path:str) -> list:
+    try:
+        file = open(path,'rb')
+    except:
+        print('File not found.')
+        return
+    return pickle.load(file)
+
+def cleanUpComments(comments:list) -> list:
+    commentsVectorList = []
+    pattern = r'[^\w]+' #pattern to detect non alpha numeric characters
+    
+    stop_words = set(stopwords.words('english'))#load nltk stop words
+    
+    for comment in comments:#loop over each comment tokenizing and removing stop words
+        comment = re.sub(pattern,' ',comment) # replace pattern with empty string
+        currentWordVector = nltk.word_tokenize(comment)
+        filteredWordVector = [w for w in currentWordVector if not w in stop_words]
+        commentsVectorList.append(filteredWordVector)#append filtered tokenized comment to final list
+    return commentsVectorList
+
+def removeClassBlocks(comments:list,codeBlocks:list):
+    commentToRemove = []
+    codeToRemove = []
+    for i,code in enumerate(codeBlocks):
+        if code.split(" ")[0] == "class":
+            commentToRemove.append(comments[i])
+            codeToRemove.append(codeBlocks[i])
+    for comment in commentToRemove:
+        comments.remove(comment)
+    for code in codeToRemove:
+        codeBlocks.remove(code)
+
+def removeBadCodeCommentPair(comments:list,codeBlocks:list):
+    fse = FunctionSignatureExtractor()
+    commentToRemove = []
+    codeToRemove = []
+    for i,code in enumerate(codeBlocks):
+        try:
+            fse.getSignature(code)
+        except:
+            commentToRemove.append(comments[i])
+            codeToRemove.append(codeBlocks[i])
+    for comment in commentToRemove:
+        comments.remove(comment)
+    for code in codeToRemove:
+        codeBlocks.remove(code)
+
+def getFunctionSignatures(codeBlocks:list)->list:
+    fse = FunctionSignatureExtractor()
+    functionSignatureList = []
+    for code in codeBlocks:
+        functionSignatureList.append(fse.getSignature(code))
+    return functionSignatureList
+
+def pickleObject(object,filename):
+    f = open(filename+".pkl",'wb')
+    pickle.dump(object,f)
+    f.close()    
+        
+comments = loadPickleFile('Pickle/scipy_comments.pkl')
+codeBlocks = loadPickleFile('Pickle/scipy_codeblocks.pkl')
+comments_tokenized = loadPickleFile('Pickle/scipy_comments_tokenized.pkl')
+functionSignatureList = loadPickleFile('Pickle/scipy_fsl.pkl')
