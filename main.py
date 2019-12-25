4d9c7c5f6dcb2e9eae8ffa4be632643d83637308
Mohamad Abou Harb <20209926+TwoShock@users.noreply.github.com>
2019-12-25 20:56:28 +0200

    added new descion tree based approach and the ultimate pkl file

--- main.py
+++ main.py
@@ -3,13 +3,53 @@ Created on Thu Nov 21 18:17:07 2019
 
 @author: Mohamad Abou Harb
 """
-import pandas as pd
+
 import re 
 import pickle
 import nltk
+import keyword
+import pandas as pd
+import math
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from utils import FunctionSignatureExtractor
+from spellchecker import SpellChecker
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO  
+from collections import Counter
+from sklearn.metrics import classification_report, confusion_matrix
+from IPython.display import Image  
+import pydotplus
+
+class MultiColumnLabelEncoder:
+    def __init__(self,columns = None):
+        self.columns = columns # array of column names to encode
+
+    def fit(self,X,y=None):
+        return self # not relevant here
+
+    def transform(self,X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                output[colname] = LabelEncoder().fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
 
 def loadPickleFile(path:str) -> list:
     try:
@@ -22,8 +62,6 @@ def loadPickleFile(path:str) -> list:
 def cleanUpComments(comments:list) -> list:
     commentsVectorList = []
     pattern = r'[^\w]+' #pattern to detect non alpha numeric characters
-    
-    stop_words = set(stopwords.words('english'))#load nltk stop words
     
     for comment in comments:#loop over each comment tokenizing and removing stop words
         comment = re.sub(pattern,' ',comment) # replace pattern with empty string
@@ -86,16 +124,17 @@ def tagComments(comments:list)->list:
 def annotateParameters(comments:list,functionSignature:list)->list:
     taggedComments = tagComments(comments)
     annotatedComments = []
+    
     for i,comment in enumerate(taggedComments):
         currentList = []
         for t in comment:
             if t[0] in functionSignature[i][1]:
-                currentList.append([t[0],'P'])
+                currentList.append([t[0],t[1],'P'])
             else:
-                currentList.append([t[0],t[1]])
+                currentList.append([t[0],t[1],'W'])
         annotatedComments.append(currentList)
-    return annotatedComments
 
+    return annotatedComments
 def createPosTagList(annotatedComments:list)->list:
     posTagList = []
     for ac in annotatedComments:
@@ -105,42 +144,180 @@ def createPosTagList(annotatedComments:list)->list:
         posTagList.append(currentPosList)
     return posTagList
 
-def getRawTriBiCounts(posTagList:list):
-    trigrams = [nltk.ngrams(posTagList[i],3) for i in range(0,410)]
-    bigrams = [nltk.ngrams(posTagList[i],2) for i in range(0,410)]
-    ttp =nltk. FreqDist()#dictionary containg count of all tag1 tag2 followed by P tag set
-    tt = nltk.FreqDist()
-    for trigram in trigrams:
-        ttp += nltk.FreqDist(trigram)
-    for bigram in bigrams:
-            tt += nltk.FreqDist(bigram)
-    return tt,ttp
-
-def predict(tokenizedComment,tt,ttp):
-    taggedComment = nltk.pos_tag(tokenizedComment)
-    pVector = []
-    for i in range(2,len(taggedComment)):
-        try:
-            ttpKey =(taggedComment[i-2][1],taggedComment[i-1][1],taggedComment[i][1])
-            ttKey = (taggedComment[i-2][1],taggedComment[i-1][1])
-            if(ttKey in tt.keys() and ttpKey in ttp.keys()):
-                p = ttp[ttpKey]/tt[ttKey]
-                pVector.append((tokenizedComment[i],p))
-            else:
-                pVector.append((tokenizedComment[i],0))
-        except Exception as e:
-            print("here",e)
-    return pVector
-        
+def reduceComments(annotatedComments:list)->list:
+    reducedComments = []
+    digitPattern = r'\d+'
+    uselessWords = ['the','is','are','am','was','there','at','on','where','was','how','which','be','can','for','an','in','of','off','The','and','but','or','their','they','can','that','as','such']
+    for ac in annotatedComments:
+        current = []
+        for w in ac:
+            if(not re.match(digitPattern,w[0]) and w[0] not in uselessWords):
+                current.append(w)
+        reducedComments.append(current)
     
+    return reducedComments
+
+def getTermFrequencyVectors(reducedComments:list)->list:
+    localTFVector = []
+    globalTFDict = {}
+    for comment in reducedComments:
+        localDict = {}
+        for w in comment:
+            if((w[0],w[2]) in globalTFDict.keys()):
+                globalTFDict[(w[0],w[2])] +=1
+            else:
+                globalTFDict[(w[0],w[2])] = 1
+            if((w[0],w[2]) in localDict.keys()):
+                localDict[w[0],w[2]] +=1
+            else:
+                localDict[w[0],w[2]] = 1
+        localTFVector.append(localDict)
+    return localTFVector,globalTFDict
+
+def tagPythonPos(annotatedComments:list):
+    pythonTypes = ['NoneType','int','long','float','double','complex','bool','str','string','unicode','array','ndarray','list','dataframe','tuple','set','map','dict','arr','shape','type']
+    pythonKeywords = keyword.kwlist
+    pythonRegexList = [r'.*(A|a)rray.*',r'.*(N|n)one.*',r'.*(E|e)rror.*',r'(I|i)nteger']
+    for comment in annotatedComments:
+        for w in comment:
+            if(w[0] in pythonTypes or w[0] in pythonKeywords):
+                w.append('PythonPos')
+            else:
+                w.append('NotPythonPos')
+            for pattern in pythonRegexList:
+                if(re.match(pattern,w[0])):
+                    if(len(w) == 4):
+                        if(w[3] == 'NotPythonPos'):
+                            w[3] = 'PythonPos'
+    return annotatedComments
+def tagLexicon(annotatedComments):
+    spell = SpellChecker()
+    for ac in annotatedComments:
+        tc = [w[0] for w in ac]
+        specialWord = spell.unknown(tc)
+    
+        for w in ac:
+            if len(w[0])<=2:
+                specialWord.add(w[0])
+        for w in ac:
+            if w[0].casefold() in map(str.casefold, specialWord):
+                w.append('special')
+            else:
+                w.append('lexicon')
+    return annotatedComments
+def convertToPandasDF(reducedComments):
+    data = {'Word':[],'POS':[],'Python POS':[],'In-Lexicon':[],'Surrounded':[],'RelativeIdx':[],'TF-IDF':[],'Output':[]}
+    for comment in reducedComments:
+        for w in comment:
+            data['Word'].append(w[0])
+            data['POS'].append(w[1])
+            if(w[2] == 'P'):
+                data['Output'].append(1)
+            else:
+                data['Output'].append(0)
+                
+            data['Python POS'].append(w[3])
+            data['In-Lexicon'].append(w[4])
+            data['Surrounded'].append(w[5])
+            data['TF-IDF'].append(w[6])
+            data['RelativeIdx'].append(w[7])
+    return pd.DataFrame(data)
+def oneHotEncode(df,col,prefix):
+    df[col] = pd.Categorical(df[col])
+    dfDummies = pd.get_dummies(df[col], prefix = prefix)
+    df = df.drop(col,axis=1)
+    df = pd.concat([df,dfDummies],axis=1)
+    return df
+def generateReducedCorpus(reducedComments:list)->list:
+    corpus = []
+    for comment in reducedComments:
+        currentComment = ""
+        for w in comment:
+            currentComment += w[0]
+            currentComment += " "
+        corpus.append(currentComment)
+    return corpus
 scipyComments = loadPickleFile('Pickle/scipy/scipy_comments_tokenized.pkl')
 scipyFSE = loadPickleFile('Pickle/scipy/scipy_fse.pkl')
+def markWordPosition(annotatedComments):
+    for comment in annotatedComments:
+        idx = 0.45*len(comment)
+        for i,w in enumerate(comment):
+            if(i < idx):
+                w.append('<40%')
+            else:
+                w.append('>40%')
+    return annotatedComments
 
-annotatedComments = annotateParameters(scipyComments,scipyFSE)
-posTagList = createPosTagList(annotatedComments)
+annotatedComments = loadPickleFile('commentDataSet.pkl')
+annotatedComments = markWordPosition(annotatedComments)
 
-from operator import itemgetter
+rc = reduceComments(annotatedComments)
 
-tt,ttp = getRawTriBiCounts(posTagList)
-pVec = predict(scipyComments[411],tt,ttp)
-pVec = sorted(pVec,key=itemgetter(1),reverse = True)
+
+
+df = convertToPandasDF(rc)
+df = MultiColumnLabelEncoder(columns = ['RelativeIdx','POS','Python POS','In-Lexicon','Surrounded','Word']).fit_transform(df)
+
+#df = df[df["Python POS"] == "NotPythonPos"]
+#df = df.drop('Python POS',axis = 1)
+#df = oneHotEncode(df,'Python POS','Python POS')
+#df = df.apply(LabelEncoder().fit_transform)
+df = df.drop('Surrounded',axis=1)
+df = df.drop('RelativeIdx',axis=1)
+##df = oneHotEncode(df,'Surrounded','Surr')
+#
+##df = df.drop('POS',axis=1)
+##df = oneHotEncode(df,'POS','POS')
+#
+##df = df.drop('In-Lexicon',axis=1)
+##df = oneHotEncode(df,'In-Lexicon','Lexicon')
+#
+#
+y = df.Output
+df = df.drop('Output',axis = 1)
+##
+df['Output'] = y
+##
+feature_cols = [col for col in df.columns]
+feature_cols.remove('Output')
+#feature_cols.remove('Word')
+X = df[feature_cols]
+####
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+##
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
+##
+##
+y_pred = clf.predict(X_test)
+##
+print('Descion Tree')
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred, average="binary"))
+print("Prescion:",metrics.precision_score(y_test, y_pred, average="binary"))
+#
+#
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+#
+
+
+
+#from sklearn.metrics import confusion_matrix
+#import matplotlib.pyplot as plt
+#
+#cm = confusion_matrix(y_test, y_pred)
+#print(cm)
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#cax = ax.matshow(cm)
+#plt.title('Confusion matrix of the classifier')
+#fig.colorbar(cax)
+#ax.set_xticklabels([''] + labels)
+#ax.set_yticklabels([''] + labels)
+#plt.xlabel('Predicted')
+#plt.ylabel('True')
+#plt.show()
+#
+#
